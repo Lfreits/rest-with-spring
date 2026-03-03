3f6c6edfa76e49390d2ad7bc7cc35c063bee8698
Lucas Freitas <freitas.lucas0402@gmail.com>
2026-03-03 10:10:44 -0300

    Tirando partes mais básicas do projeto. Mantendo somente o que for ligado ao Person, que é como estamos trabalhando com JPA

--- src/main/java/br/com/techflowhub/exception/handler/CustomEntityResponseHandler.java
+++ src/main/java/br/com/techflowhub/exception/handler/CustomEntityResponseHandler.java
@@ -2,7 +2,6 @@ package br.com.techflowhub.exception.handler;
 
 import br.com.techflowhub.exception.ExceptionResponse;
 import br.com.techflowhub.exception.ResourceNotFoundException;
-import br.com.techflowhub.exception.UnsupportedMathOperationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -25,15 +24,6 @@ public class CustomEntityResponseHandler extends ResponseEntityExceptionHandler 
                 request.getDescription(false)
         );
         return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
-    }
-    @ExceptionHandler(UnsupportedMathOperationException.class)
-    public final ResponseEntity<ExceptionResponse> handleBadRequestExceptions(Exception ex, WebRequest request) {
-        ExceptionResponse response = new ExceptionResponse(
-                new Date(),
-                ex.getMessage(),
-                request.getDescription(false)
-        );
-        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(ResourceNotFoundException.class)
