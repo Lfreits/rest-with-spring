48e31d7692833feaf9c191f0ad7599b903d17bb3
Lucas Freitas <freitas.lucas0402@gmail.com>
2026-03-06 14:54:50 -0300

    Implementando Content Negociation via QUERY Parameter. Agora responde em JSON ou em XML

--- src/main/java/br/com/techflowhub/controllers/PersonController.java
+++ src/main/java/br/com/techflowhub/controllers/PersonController.java
@@ -19,7 +19,7 @@ public class PersonController {
     private PersonServices service;
 
     @GetMapping(
-            produces = MediaType.APPLICATION_JSON_VALUE
+            produces = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE }
     )
     public List<PersonDTO> findAll() {
         return service.findAll();
@@ -27,23 +27,23 @@ public class PersonController {
 
     @GetMapping(
             value = "/{id}",
-            produces = MediaType.APPLICATION_JSON_VALUE
+            produces = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE }
     )
     public PersonDTO findById(@PathVariable("id") Long id) {
         return service.findById(id);
     }
 
     @PostMapping(
-            consumes = MediaType.APPLICATION_JSON_VALUE,
-            produces = MediaType.APPLICATION_JSON_VALUE
+            consumes = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE },
+            produces = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE }
     )
     public PersonDTO create(@RequestBody PersonDTO person) {
         return service.create(person);
     }
 
     @PutMapping(
-            consumes = MediaType.APPLICATION_JSON_VALUE,
-            produces = MediaType.APPLICATION_JSON_VALUE
+            consumes = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE },
+            produces = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE }
     )
     public PersonDTO update(@RequestBody PersonDTO person) {
         return service.update(person);
