092578d6ba34e443225804562912196e412f215b
Lucas Freitas <freitas.lucas0402@gmail.com>
2026-03-11 16:25:36 -0300

    Implementando Content Negociation via HEADER Parameter.

--- src/main/java/br/com/techflowhub/config/WebConfig.java
+++ src/main/java/br/com/techflowhub/config/WebConfig.java
@@ -10,9 +10,19 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
-        configurer.favorParameter(true)
-                .parameterName("mediaType")
-                .ignoreAcceptHeader(true)
+
+        // Via QUERY PARAM http://localhost/api/person/v1/2?MediaType=xml
+//        configurer.favorParameter(true)
+//                .parameterName("mediaType")
+//                .ignoreAcceptHeader(true)
+//                .useRegisteredExtensionsOnly(false)
+//                .defaultContentType(MediaType.APPLICATION_JSON)
+//                .mediaType("json", MediaType.APPLICATION_JSON)
+//                .mediaType("xml", MediaType.APPLICATION_XML);
+
+            // Via HEADER PARAM http://localhost/api/person/v1/2
+            configurer.favorParameter(false)
+                .ignoreAcceptHeader(false)
                 .useRegisteredExtensionsOnly(false)
                 .defaultContentType(MediaType.APPLICATION_JSON)
                 .mediaType("json", MediaType.APPLICATION_JSON)
