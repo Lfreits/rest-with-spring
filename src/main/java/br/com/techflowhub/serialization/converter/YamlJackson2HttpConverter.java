e98a5cc7fc7cc262f3de5c02c5634c6f4fbb6a5c
Lucas Freitas <freitas.lucas0402@gmail.com>
2026-03-12 16:00:08 -0300

    Trabalhando com testes unitários usando Mockito

---
+++ src/main/java/br/com/techflowhub/serialization/converter/YamlJackson2HttpConverter.java
@@ -0,0 +1,15 @@
+package br.com.techflowhub.serialization.converter;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.dataformat.yaml.YAMLMapper;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.json.AbstractJackson2HttpMessageConverter;
+
+public final class YamlJackson2HttpConverter extends AbstractJackson2HttpMessageConverter {
+
+    YamlJackson2HttpConverter() {
+        super(new YAMLMapper()
+                .setSerializationInclusion(JsonInclude.Include.NON_NULL),
+                    MediaType.parseMediaType("application/yaml"));
+    }
+}
